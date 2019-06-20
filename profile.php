ca3ff0b23cf782524d69cf075644a8e3a2e85654
DevelWanderer <52003425+DevelWanderer@users.noreply.github.com>
2019-06-20 13:51:02 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,7 +1,7 @@
 <?php
 $access_token = '/5qKcInqTBGTrFAd52HnHFREKSsP2CHN07FK8036ALc7U5m6nmYJueTRYuMoAGoseez7KarRqVmm/0MByL+T81/fX1Ze7PLk12uaKfu2CqOigopGOB4QBZOIVG3CGoqVYvRACqqhZueFLmndOoWwzwdB04t89/1O/w1cDnyilFU=';
-$userId = 'U247e07dbd7112244b44c934915d5aceb';
-$url = 'https://api.line.me/v2/bot/profile/'.$userId;
+$id = $arrayJson['events'][0]['source']['userId'];
+$url = 'https://api.line.me/v2/bot/profile/'.$id;
 $headers = array('Authorization: Bearer ' . $access_token);
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
