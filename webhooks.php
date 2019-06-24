fc1a814511e1a2966e802451c01f75644259b75d
DevelWanderer <52003425+DevelWanderer@users.noreply.github.com>
2019-06-24 12:19:14 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,38 +1,14 @@
-<?php
-/// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
-$CHANNEL_ID = '1590205061';
-$CHANNEL_SECRET='1d07c3906c0ce7e7f7cf71b0f20e10bc';
-$ACCESS_TOKEN ='/5qKcInqTBGTrFAd52HnHFREKSsP2CHN07FK8036ALc7U5m6nmYJueTRYuMoAGoseez7KarRqVmm/0MByL+T81/fX1Ze7PLk12uaKfu2CqOigopGOB4QBZOIVG3CGoqVYvRACqqhZueFLmndOoWwzwdB04t89/1O/w1cDnyilFU=');
-require "vendor/autoload.php";
-$content = file_get_contents('php://input');
-$events = json_decode($content, true);
-if(!is_null($events)){
-    // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน
-    $replyToken = $events['events'][0]['replyToken'];
-    $typeMessage = $events['events'][0]['message']['type'];
-    $userMessage = $events['events'][0]['message']['text'];
-    switch ($typeMessage){
-        case 'text':
-            switch ($userMessage) {
-                case "สวัสดี":
-                    $textReplyMessage = "สวัสดีฮัฟ";
-                    break;
-                case "ทักๆ":
-                    $textReplyMessage = "ทักเช่นกันฮัฟ";
-                    break;
-                default:
-                    $textReplyMessage = "สวัสดีฮัฟ";
-                    break;
-            }
-            break;
-        default:
-            $textReplyMessage = json_encode($events);
-            break;
-    }
+<?php // callback.php
+$ composer require linecorp/line-bot-sdk
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
+$replyToken = $events['events'][0]['replyToken'];
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+$response = $bot->replyMessage($replyToken, $textMessageBuilder);
+if ($response->isSucceeded()) {
+    echo 'Succeeded!';
+    return;
 }
-// ส่วนของคำสั่งจัดเตียมรูปแบบข้อความสำหรับส่ง
-$textMessageBuilder = new TextMessageBuilder($textReplyMessage);
 
-//l ส่วนของคำสั่งตอบกลับข้อความ
-$response = $bot->replyMessage($replyToken,$textMessageBuilder);
-?>
+// Failed
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
