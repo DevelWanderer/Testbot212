86e3dd1de4691725b37f9569a8c1ee3c907edd30
DevelWanderer <52003425+DevelWanderer@users.noreply.github.com>
2019-06-24 11:12:36 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -7,20 +7,35 @@ $id = $arrayJson['events'][0]['source']['userId'];
 $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-// Loop through each event
-foreach ($events['events'] as $event) {
-// Reply only when message sent is in 'text' format
-if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-// Get text sent
-$text = $event['source']['$id'];
-// Get replyToken
-$replyToken = $event['replyToken'];
-// Build message to reply back
-$messages = [
-'type' => 'text',
-'text' => 'สวัสดีครับ',$id
+if(!is_null($events)){
+    // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน
+    $replyToken = $events['events'][0]['replyToken'];
+    $typeMessage = $events['events'][0]['message']['type'];
+    $userMessage = $events['events'][0]['message']['text'];
+    switch ($typeMessage){
+        case 'text':
+            switch ($userMessage) {
+                case "สวัสดี":
+                    $textReplyMessage = "สวัสดีฮัฟ";
+                    break;
+                case "ทักๆ":
+                    $textReplyMessage = "ทักเช่นกันฮัฟ";
+                    break;
+                default:
+                    $textReplyMessage = "สวัสดีฮัฟ";
+                    break;
+            }
+            break;
+        default:
+            $textReplyMessage = json_encode($events);
+            break;
+    }
+}
+// ส่วนของคำสั่งจัดเตียมรูปแบบข้อความสำหรับส่ง
+$textMessageBuilder = new TextMessageBuilder($textReplyMessage);
+
+//l ส่วนของคำสั่งตอบกลับข้อความ
+$response = $bot->replyMessage($replyToken,$textMessageBuilder);
 ];
 // Make a POST Request to Messaging API to reply to sender
 $url = 'https://api.line.me/v2/bot/message/reply';
