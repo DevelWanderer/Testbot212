2a4acc5568f8c8115d82ff0dadddccc4285458fc
DevelWanderer <52003425+DevelWanderer@users.noreply.github.com>
2019-06-24 11:07:56 +0700

    Update webhooks2.php

--- webhooks2.php
+++ webhooks2.php
@@ -4,7 +4,7 @@ $CHANNEL_ID = '1590205061';
 $CHANNEL_SECRET='1d07c3906c0ce7e7f7cf71b0f20e10bc';
 $ACCESS_TOKEN ='/5qKcInqTBGTrFAd52HnHFREKSsP2CHN07FK8036ALc7U5m6nmYJueTRYuMoAGoseez7KarRqVmm/0MByL+T81/fX1Ze7PLk12uaKfu2CqOigopGOB4QBZOIVG3CGoqVYvRACqqhZueFLmndOoWwzwdB04t89/1O/w1cDnyilFU=');
 require "vendor/autoload.php";
-
+$content = file_get_contents('php://input');
 $events = json_decode($content, true);
 if(!is_null($events)){
     // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน
@@ -35,4 +35,24 @@ $textMessageBuilder = new TextMessageBuilder($textReplyMessage);
 
 //l ส่วนของคำสั่งตอบกลับข้อความ
 $response = $bot->replyMessage($replyToken,$textMessageBuilder);
+$url = 'https://api.line.me/v2/bot/message/reply';
+$data = [
+'replyToken' => $replyToken,
+'messages' => [$messages],
+];
+$post = json_encode($data);
+$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+echo $result . "\r\n";
+}
+}
+}
+echo "OK";
 ?>
