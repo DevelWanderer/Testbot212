5e681a5c36f37c2532d085e71fa9447ae8ff0a41
DevelWanderer <52003425+DevelWanderer@users.noreply.github.com>
2019-06-24 16:38:08 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,12 +3,12 @@
 	$datas = file_get_contents('php://input');
 	/*Decode Json From LINE Data Body*/
 	$deCode = json_decode($datas,true);
-
-	file_put_contents('log.txt', file_get_contents('php://input') . PHP_EOL, FILE_APPEND);
 	$res = $bot->getProfile($userId);
 	if ($res->isSucceeded()) {
 	$profile = $res->getJSONDecodedBody();
 	$displayName = $profile['displayName'];
+	file_put_contents('log.txt', file_get_contents('php://input') . PHP_EOL, FILE_APPEND);
+	
 	$replyToken = $deCode['events'][0]['replyToken'];
 	}
 	$messages = [];
