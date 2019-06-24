b7a851f98cb86636ed524046b446c9e9b79ec8df
DevelWanderer <52003425+DevelWanderer@users.noreply.github.com>
2019-06-24 11:58:49 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,7 +1,7 @@
 <?php // callback.php
 $ composer require linecorp/line-bot-sdk
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('</5qKcInqTBGTrFAd52HnHFREKSsP2CHN07FK8036ALc7U5m6nmYJueTRYuMoAGoseez7KarRqVmm/0MByL+T81/fX1Ze7PLk12uaKfu2CqOigopGOB4QBZOIVG3CGoqVYvRACqqhZueFLmndOoWwzwdB04t89/1O/w1cDnyilFU=>');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<1d07c3906c0ce7e7f7cf71b0f20e10bc>']);
 
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
 $response = $bot->replyMessage('<reply token>', $textMessageBuilder);
