3323c951d7e1531278ef5b8c22a407906846524a
DevelWanderer <52003425+DevelWanderer@users.noreply.github.com>
2019-06-24 11:06:36 +0700

    Update webhooks2.php

--- webhooks2.php
+++ webhooks2.php
@@ -1,8 +1,8 @@
 <?php
 /// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
-define('LINE_MESSAGE_CHANNEL_ID','1590205061');
-define('LINE_MESSAGE_CHANNEL_SECRET','1d07c3906c0ce7e7f7cf71b0f20e10bc');
-define('LINE_MESSAGE_ACCESS_TOKEN','/5qKcInqTBGTrFAd52HnHFREKSsP2CHN07FK8036ALc7U5m6nmYJueTRYuMoAGoseez7KarRqVmm/0MByL+T81/fX1Ze7PLk12uaKfu2CqOigopGOB4QBZOIVG3CGoqVYvRACqqhZueFLmndOoWwzwdB04t89/1O/w1cDnyilFU=');
+$CHANNEL_ID = '1590205061';
+$CHANNEL_SECRET='1d07c3906c0ce7e7f7cf71b0f20e10bc';
+$ACCESS_TOKEN ='/5qKcInqTBGTrFAd52HnHFREKSsP2CHN07FK8036ALc7U5m6nmYJueTRYuMoAGoseez7KarRqVmm/0MByL+T81/fX1Ze7PLk12uaKfu2CqOigopGOB4QBZOIVG3CGoqVYvRACqqhZueFLmndOoWwzwdB04t89/1O/w1cDnyilFU=');
 require "vendor/autoload.php";
 
 $events = json_decode($content, true);
