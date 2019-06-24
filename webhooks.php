86162282ea72e1773f8dec9661caf81ed931e68f
DevelWanderer <52003425+DevelWanderer@users.noreply.github.com>
2019-06-24 11:45:05 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -8,8 +8,19 @@ $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
-new TextMessageBuilder("คุณ ".$profile["displayName"]."\nเพศ: $genderemo ".$gendertext."\nเกิดวันที่ $bd"))
-];
+if (!is_null($events['events'])) {
+// Loop through each event
+foreach ($events['events'] as $event) {
+// Reply only when message sent is in 'text' format
+if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+// Get text sent
+$text = $event['source']['$id'];
+// Get replyToken
+$replyToken = $event['replyToken'];
+// Build message to reply back
+$messages = 
+new TextMessageBuilder("คุณ ".$profile["displayName"]."\nเพศ: $genderemo ".$gendertext."\nเกิดวันที่ $bd"));
+
 // Make a POST Request to Messaging API to reply to sender
 $url = 'https://api.line.me/v2/bot/message/reply';
 $data = [
