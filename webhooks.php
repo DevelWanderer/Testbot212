91c421c259e1d081666f13a3b686fa1bd082d5c2
DevelWanderer <52003425+DevelWanderer@users.noreply.github.com>
2019-06-24 11:09:11 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,28 +1,40 @@
-<?php // callback.php
-//require "vendor/autoload.php";
-//require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
-$access_token = '/5qKcInqTBGTrFAd52HnHFREKSsP2CHN07FK8036ALc7U5m6nmYJueTRYuMoAGoseez7KarRqVmm/0MByL+T81/fX1Ze7PLk12uaKfu2CqOigopGOB4QBZOIVG3CGoqVYvRACqqhZueFLmndOoWwzwdB04t89/1O/w1cDnyilFU=';
-$id = $arrayJson['events'][0]['source']['userId'];
-// Get POST body content
+<?php
+/// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
+$CHANNEL_ID = '1590205061';
+$CHANNEL_SECRET='1d07c3906c0ce7e7f7cf71b0f20e10bc';
+$ACCESS_TOKEN ='/5qKcInqTBGTrFAd52HnHFREKSsP2CHN07FK8036ALc7U5m6nmYJueTRYuMoAGoseez7KarRqVmm/0MByL+T81/fX1Ze7PLk12uaKfu2CqOigopGOB4QBZOIVG3CGoqVYvRACqqhZueFLmndOoWwzwdB04t89/1O/w1cDnyilFU=');
+require "vendor/autoload.php";
 $content = file_get_contents('php://input');
-// Parse JSON
 $events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-// Loop through each event
-foreach ($events['events'] as $event) {
-// Reply only when message sent is in 'text' format
-if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-// Get text sent
-$text = $event['source']['userId'];
-// Get replyToken
-$replyToken = $event['replyToken'];
-// Build message to reply back
-$messages = [
-'type' => 'text',
-'text' => 'สวัสดีครับ',$id
-];
-// Make a POST Request to Messaging API to reply to sender
+if(!is_null($events)){
+    // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน
+    $replyToken = $events['events'][0]['replyToken'];
+    $typeMessage = $events['events'][0]['message']['type'];
+    $userMessage = $events['events'][0]['message']['text'];
+    switch ($typeMessage){
+        case 'text':
+            switch ($userMessage) {
+                case "สวัสดี":
+                    $textReplyMessage = "สวัสดีฮัฟ";
+                    break;
+                case "ทักๆ":
+                    $textReplyMessage = "ทักเช่นกันฮัฟ";
+                    break;
+                default:
+                    $textReplyMessage = "สวัสดีฮัฟ";
+                    break;
+            }
+            break;
+        default:
+            $textReplyMessage = json_encode($events);
+            break;
+    }
+}
+// ส่วนของคำสั่งจัดเตียมรูปแบบข้อความสำหรับส่ง
+$textMessageBuilder = new TextMessageBuilder($textReplyMessage);
+
+//l ส่วนของคำสั่งตอบกลับข้อความ
+$response = $bot->replyMessage($replyToken,$textMessageBuilder);
 $url = 'https://api.line.me/v2/bot/message/reply';
 $data = [
 'replyToken' => $replyToken,
@@ -43,3 +55,4 @@ echo $result . "\r\n";
 }
 }
 echo "OK";
+?>
