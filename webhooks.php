9c0d2a7c6b298a736291d959412bc8da7790fa05
DevelWanderer <52003425+DevelWanderer@users.noreply.github.com>
2019-06-25 15:11:06 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 define('LINE_MESSAGE_CHANNEL_ID','1590205061');
 define('LINE_MESSAGE_CHANNEL_SECRET','1d07c3906c0ce7e7f7cf71b0f20e10bc');
 define('LINE_MESSAGE_ACCESS_TOKEN','/5qKcInqTBGTrFAd52HnHFREKSsP2CHN07FK8036ALc7U5m6nmYJueTRYuMoAGoseez7KarRqVmm/0MByL+T81/fX1Ze7PLk12uaKfu2CqOigopGOB4QBZOIVG3CGoqVYvRACqqhZueFLmndOoWwzwdB04t89/1O/w1cDnyilFU=');
-require_once '../vendor/autoload.php';
+
 $httpClient = new CurlHTTPClient(LINE_MESSAGE_ACCESS_TOKEN);
 $bot = new LINEBot($httpClient, array('channelSecret' => LINE_MESSAGE_CHANNEL_SECRET));
 $content = file_get_contents('php://input');
