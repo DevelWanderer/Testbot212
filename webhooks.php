a5c5152ce3a3600c24cedfb416009506cba2f624
DevelWanderer <52003425+DevelWanderer@users.noreply.github.com>
2019-06-25 12:04:49 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,8 @@
 	$datas = file_get_contents('php://input');
 	/*Decode Json From LINE Data Body*/
 	$deCode = json_decode($datas,true);
-
+	curl -v -X GET https://api.line.me/v2/profile \
+	-H 'Authorization: Bearer {access token}
 	file_put_contents('log.txt', file_get_contents('php://input') . PHP_EOL, FILE_APPEND);
 
 	$replyToken = $deCode['events'][0]['replyToken'];
