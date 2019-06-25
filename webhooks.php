c988fdf5ac81376f5852b727d5906ee076b63601
DevelWanderer <52003425+DevelWanderer@users.noreply.github.com>
2019-06-25 11:46:29 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,31 +1,27 @@
-<?php 
+<?php
 	/*Get Data From POST Http Request*/
 	$datas = file_get_contents('php://input');
 	/*Decode Json From LINE Data Body*/
-	$deCode = json_decode($datas,true);<?php // callback.php
-require "vendor/autoload.php";
-require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
-$access_token = '/5qKcInqTBGTrFAd52HnHFREKSsP2CHN07FK8036ALc7U5m6nmYJueTRYuMoAGoseez7KarRqVmm/0MByL+T81/fX1Ze7PLk12uaKfu2CqOigopGOB4QBZOIVG3CGoqVYvRACqqhZueFLmndOoWwzwdB04t89/1O/w1cDnyilFU=';
+	$deCode = json_decode($datas,true);
 
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
+	file_put_contents('log.txt', file_get_contents('php://input') . PHP_EOL, FILE_APPEND);
+	$replyToken = $deCode['events'][0]['replyToken'];
+	$events = json_decode($content, true);
 // Validate parsed JSON data
-if (!is_null($events['events'])) {
+	if (!is_null($events['events'])) {
 // Loop through each event
-foreach ($events['events'] as $event) {
+	foreach ($events['events'] as $event) {
 // Reply only when message sent is in 'text' format
-if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+	if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 // Get text sent
-$text = $event['source']['userId'];
+	$text = $event['source']['userId'];
 // Get replyToken
-$replyToken = $event['replyToken'];
+	$replyToken = $event['replyToken'];
 // Build message to reply back
-$messages = [
-'type' => 'text',
-'text' => $text
-];
+	$messages = [
+		'type' => 'text',
+		'text' => $text
+	];
 // Make a POST Request to Messaging API to reply to sender
 $url = 'https://api.line.me/v2/bot/message/reply';
 $data = [
@@ -33,19 +29,63 @@ $data = [
 'messages' => [$messages],
 ];
 $post = json_encode($data);
+
+		$LINEDatas['url'] = "https://api.line.me/v2/bot/message/reply";
+  	$LINEDatas['token'] = "/5qKcInqTBGTrFAd52HnHFREKSsP2CHN07FK8036ALc7U5m6nmYJueTRYuMoAGoseez7KarRqVmm/0MByL+T81/fX1Ze7PLk12uaKfu2CqOigopGOB4QBZOIVG3CGoqVYvRACqqhZueFLmndOoWwzwdB04t89/1O/w1cDnyilFU=";
+		$LINEprofile['profile'] ="https://api.line.me/v2/bot/profile/{userId}"
+  	$results = sentMessage($encodeJson,$LINEDatas);
+
+	/*Return HTTP Request 200*/
 	http_response_code(200);
-$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
-echo $result . "\r\n";
-}
-}
-}
+
+	function getFormatTextMessage($text)
+	{
+		$datas = [];
+		$datas['type'] = 'text';
+		$datas['text'] = $text;
+
+		return $datas;
+	}
+
+	function sentMessage($encodeJson,$datas)
+	{
+		$datasReturn = [];
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+		  CURLOPT_URL => $datas['url'],
+		  CURLOPT_RETURNTRANSFER => true,
+		  CURLOPT_ENCODING => "",
+		  CURLOPT_MAXREDIRS => 10,
+		  CURLOPT_TIMEOUT => 30,
+		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		  CURLOPT_CUSTOMREQUEST => "POST",
+		  CURLOPT_POSTFIELDS => $encodeJson,
+		  CURLOPT_HTTPHEADER => array(
+		    "authorization: Bearer ".$datas['token'],
+		    "cache-control: no-cache",
+		    "content-type: application/json; charset=UTF-8",
+		  ),
+		));
+
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+
+		curl_close($curl);
+
+		if ($err) {
+		    $datasReturn['result'] = 'E';
+		    $datasReturn['message'] = $err;
+		} else {
+		    if($response == "{}"){
+			$datasReturn['result'] = 'S';
+			$datasReturn['message'] = 'Success';
+		    }else{
+			$datasReturn['result'] = 'E';
+			$datasReturn['message'] = $response;
+		    }
+		}
+
+		return $datasReturn;
+	}
 echo "OK";
-	
+?>
