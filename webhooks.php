d51498515e43104d2f0d4b722b2255453d9ff17d
DevelWanderer <52003425+DevelWanderer@users.noreply.github.com>
2019-06-25 12:43:52 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,14 +1,64 @@
 <?php
-date_default_timezone_set(“Asia/Bangkok”);
-$date = date(“Y-m-d”);
-$time = date(“H:i:s”);
-$json = file_get_contents('php://input');
-$request = json_decode($json, true);
-$queryText = $request[“queryResult”][“queryText”];
-$userId = $request[‘originalDetectIntentRequest’][‘payload’][‘data’][‘source’][‘userId’];
-$myfile = fopen(“log$date.txt”, “a”) or die(“Unable to open file!”);
-$log = $date.”-”.$time.”\t”.$userId.”\t”.$queryText.”\n”;
-fwrite($myfile,$log);
-fclose($myfile);
-return res.status(200).send(req.method);
-?>
+require "vendor/autoload.php";
+require_once 'botpush.php';
+$httpClient = new CurlHTTPClient(LINE_MESSAGE_ACCESS_TOKEN);
+$bot = new LINEBot($httpClient, array('channelSecret' => LINE_MESSAGE_CHANNEL_SECRET));
+$content = file_get_contents('php://input');
+$hash = hash_hmac('sha256', $content, LINE_MESSAGE_CHANNEL_SECRET, true);
+$signature = base64_encode($hash);
+$events = $bot->parseEventRequest($content, $signature);
+$eventObj = $events[0]; // Event Object ของ array แรก
+$eventType = $eventObj->getType();
+$userId = NULL;
+$groupId = NULL;
+$roomId = NULL;
+$sourceId = NULL;
+$sourceType = NULL;
+// สร้างตัวแปร replyToken และ replyData สำหรับกรณีใช้ตอบกลับข้อความ
+$replyToken = NULL;
+$replyData = NULL;
+// สร้างตัวแปร ไว้เก็บค่าว่าเป้น Event ประเภทไหน
+$eventMessage = NULL;
+$eventPostback = NULL;
+$eventJoin = NULL;
+$eventLeave = NULL;
+$eventFollow = NULL;
+$eventUnfollow = NULL;
+$eventBeacon = NULL;
+$eventAccountLink = NULL;
+$eventMemberJoined = NULL;
+$eventMemberLeft = NULL;
+switch($eventType){
+    case 'message': $eventMessage = true; break;    
+    case 'postback': $eventPostback = true; break;  
+    case 'join': $eventJoin = true; break;  
+    case 'leave': $eventLeave = true; break;    
+    case 'follow': $eventFollow = true; break;  
+    case 'unfollow': $eventUnfollow = true; break;  
+    case 'beacon': $eventBeacon = true; break;     
+    case 'accountLink': $eventAccountLink = true; break;       
+    case 'memberJoined': $eventMemberJoined = true; break;       
+    case 'memberLeft': $eventMemberLeft = true; break;     
+if($eventObj->isUserEvent()){
+        $userId = $eventObj->getUserId();  
+        $sourceType = "USER";
+if ($response->isSucceeded()) {
+        $userData = $response->getJSONDecodedBody(); // return array     
+        // $userData['userId']
+        // $userData['displayName']
+        // $userData['pictureUrl']
+        // $userData['statusMessage']
+      $textReplyMessage = 'สวัสดีครับ คุณ '.$userData['displayName'];     
+      }else{
+      $textReplyMessage = 'สวัสดีครับ คุณคือใคร';
+      }
+      $replyData = new TextMessageBuilder($textReplyMessage);                                                 
+      break;   
+$response = $bot->replyMessage($replyToken,$replyData);
+if ($response->isSucceeded()) {
+      echo 'Succeeded!';
+      return;
+      }
+      // Failed
+      echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+      ?> 
